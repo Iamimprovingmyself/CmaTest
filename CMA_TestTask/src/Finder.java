c0220b78853bd41269ef85303334db1711998370
Khasan Zhuraev <xasan1999@gmail.com>
2020-11-12 08:40:38 +0300

    улучшил способ определения пути распаковки

--- CMA_TestTask/src/Finder.java
+++ CMA_TestTask/src/Finder.java
@@ -3,11 +3,12 @@ import java.util.List;
 
 public class Finder {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         FileFinder fileFinder = new FileFinder(); //экземпляр для поиска
-        System.out.println("Введите путь к корневой папке");
-        String startPath = reader.readLine(); // путь к корневой папке
+        String userName = System.getProperty("user.name");
+        String path1 ="//Desktop//result.txt";
+        String path2 = "C://Users//";
+        String resultPath = path2 + userName + path1;
+        String startPath = "C:\\Users\\Jarvis\\Desktop\\try"; // путь к корневой папке
         try {
             List result = fileFinder.findFiles(startPath); // находим нужные файлы в лист
             FileSorter fileSorter = new FileSorter(); // экземпляр для сортировки
@@ -20,7 +21,7 @@ public class Finder {
             for(Object f : result) {
                 FileInputStream in = new FileInputStream((File) f); // берем объекты из списка
                 //записываем данные из предыдущего файла в result файл.
-                FileOutputStream out = new FileOutputStream("C://Users//Jarvis//Desktop//result.txt", true);
+                FileOutputStream out = new FileOutputStream(resultPath, true);
                 byte[] buffer = new byte[in.available()];
                 in.read(buffer, 0, buffer.length);
                 out.write(buffer, 0, buffer.length);
